fc9eecacbe8c93d6ed8793baae263c7530f0b24d
jbock <julien.bock@decathlon.com>
2018-11-06 16:33:20 +0100

    Add blog in front

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -43,31 +43,43 @@ class DefaultController extends Controller
             }
         }
 
-        $lastArticle = $em->getRepository('CoreBundle:Article')->findBy( array() ,array('date' => 'desc'), 4, 0);
+        $lastArticles = $em->getRepository('CoreBundle:Article')->findBy( array() ,array('date' => 'desc'), 4, 0);
 
-        return $this->render('@Front/Default/index.html.twig', ['lastArticle' => $lastArticle, 'articles' => $output, 'CoinFooter' => $coinFooter]);
+        return $this->render('@Front/Default/index.html.twig', ['lastArticles' => $lastArticles, 'articles' => $output, 'CoinFooter' => $coinFooter]);
+    }
+
+    /**
+     * @Route("/article/{idArticle}", name="view_article")
+     */
+    public function showArticleAction($idArticle)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $articleContent = $em->getRepository('CoreBundle:Article')->getArticleWithCommentAndNoComment($idArticle);
+
+
+        return $this->render('@Front/Default/show.html.twig', ['articleContent' => $articleContent]);
     }
 
     /**
      * @Route("/bitcoin", name="default_bitcoin")
      */
 
-     public function show_bitcoin(){
+     public function show_bitcoinAction(){
 
-        // $date = new \DateTime;
-        // $yesterday = $date->modify('-1 days')->format('Y-m-d\TH:i:s\Z');
+        $date = new \DateTime;
+        $yesterday = $date->modify('-1 days')->format('Y-m-d\TH:i:s\Z');
 
-        // $headers = array('Accept' => 'application/json', 'X-CoinAPI-Key' => '044A1C06-AFE6-4EF3-9223-0363770C3E25');
+        $headers = array('Accept' => 'application/json', 'X-CoinAPI-Key' => '044A1C06-AFE6-4EF3-9223-0363770C3E25');
                 
-        // $responseValYest = Request::get('https://rest.coinapi.io/v1/exchangerate/BTC/EUR?time='.$yesterday,$headers);
-        // $responseNow = Request::get('https://rest.coinapi.io/v1/exchangerate/BTC/EUR',$headers);
+        $responseValYest = Request::get('https://rest.coinapi.io/v1/exchangerate/BTC/EUR?time='.$yesterday,$headers);
+        $responseNow = Request::get('https://rest.coinapi.io/v1/exchangerate/BTC/EUR',$headers);
 
 
-        // $valLast = round($responseValYest->body->rate, 2);
-        // $newVal = round($responseNow->body->rate, 2);
-        // $rateChange = (($newVal - $valLast) / $newVal)*100;
+        $valLast = round($responseValYest->body->rate, 2);
+        $newVal = round($responseNow->body->rate, 2);
+        $rateChange = (($newVal - $valLast) / $newVal)*100;
 
-        return $this->render('@Front/Default/bitcoin.html.twig', ['BTCInfo' => null, 'BTCRate' => null]);
+        return $this->render('@Front/Default/bitcoin.html.twig', ['BTCInfo' => $newVal, 'BTCRate' => $rateChange]);
      }
 
      
@@ -75,22 +87,23 @@ class DefaultController extends Controller
      * @Route("/litecoin", name="default_litecoin")
      */
 
-    public function show_litecoin(){
-        // $date = new \DateTime;
-        // $yesterday = $date->modify('-1 days')->format('Y-m-d\TH:i:s\Z');
 
-        // $headers = array('Accept' => 'application/json', 'X-CoinAPI-Key' => '044A1C06-AFE6-4EF3-9223-0363770C3E25');
+    public function show_litecoinAction(){
+        $date = new \DateTime;
+        $yesterday = $date->modify('-1 days')->format('Y-m-d\TH:i:s\Z');
+
+         $headers = array('Accept' => 'application/json', 'X-CoinAPI-Key' => '044A1C06-AFE6-4EF3-9223-0363770C3E25');
                 
-        // $responseValYest = Request::get('https://rest.coinapi.io/v1/exchangerate/LTC/EUR?time='.$yesterday,$headers);
-        // $responseNow = Request::get('https://rest.coinapi.io/v1/exchangerate/LTC/EUR',$headers);
+         $responseValYest = Request::get('https://rest.coinapi.io/v1/exchangerate/LTC/EUR?time='.$yesterday,$headers);
+        $responseNow = Request::get('https://rest.coinapi.io/v1/exchangerate/LTC/EUR',$headers);
 
 
-        // $valLast = round($responseValYest->body->rate, 2);
-        // $newVal = round($responseNow->body->rate, 2);
-        // $rateChange = (($newVal - $valLast) / $newVal)*100;
+        $valLast = round($responseValYest->body->rate, 2);
+        $newVal = round($responseNow->body->rate, 2);
+        $rateChange = (($newVal - $valLast) / $newVal)*100;
 
 
-        return $this->render('@Front/Default/litecoin.html.twig', ['LTCInfo' => null, 'LTCRate' => null]);
+        return $this->render('@Front/Default/litecoin.html.twig', ['LTCInfo' => $newVal, 'LTCRate' => $rateChange]);
      }
 
      
@@ -98,29 +111,29 @@ class DefaultController extends Controller
      * @Route("/ethereum", name="default_ethereum")
      */
 
-    public function show_ethereum(){
+    public function show_ethereumAction(){
 
-        //$date = new \DateTime;
-        //$yesterday = $date->modify('-1 days')->format('Y-m-d\TH:i:s\Z');
+        $date = new \DateTime;
+        $yesterday = $date->modify('-1 days')->format('Y-m-d\TH:i:s\Z');
 
-        //$headers = array('Accept' => 'application/json', 'X-CoinAPI-Key' => '044A1C06-AFE6-4EF3-9223-0363770C3E25');
+        $headers = array('Accept' => 'application/json', 'X-CoinAPI-Key' => '044A1C06-AFE6-4EF3-9223-0363770C3E25');
                 
-        //$responseValYest = Request::get('https://rest.coinapi.io/v1/exchangerate/ETH/EUR?time='.$yesterday,$headers);
-        //$responseNow = Request::get('https://rest.coinapi.io/v1/exchangerate/ETH/EUR',$headers);
+        $responseValYest = Request::get('https://rest.coinapi.io/v1/exchangerate/ETH/EUR?time='.$yesterday,$headers);
+        $responseNow = Request::get('https://rest.coinapi.io/v1/exchangerate/ETH/EUR',$headers);
 
 
-        //$valLast = round($responseValYest->body->rate, 2);
-        //$newVal = round($responseNow->body->rate, 2);
-        //$rateChange = (($newVal - $valLast) / $newVal)*100;
+        $valLast = round($responseValYest->body->rate, 2);
+        $newVal = round($responseNow->body->rate, 2);
+        $rateChange = (($newVal - $valLast) / $newVal)*100;
 
-        return $this->render('@Front/Default/ethereum.html.twig', ['ETHInfo' => null, 'ETHRate' => null]);
+        return $this->render('@Front/Default/ethereum.html.twig', ['ETHInfo' => $newVal, 'ETHRate' => $rateChange]);
      }
 
      /**
       * @Route("/blockchain", name="default_blockchain")
       */
 
-      public function show_blockchain(){
+      public function show_blockchainAction(){
       
         return $this->render('@Front/Default/blockchain.html.twig');
       
